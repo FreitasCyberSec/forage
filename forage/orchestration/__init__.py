83797c603741aefbd6a57fec2462eb52b56c7f75
Humberto Freitas <162592407+FreitasCyberSec@users.noreply.github.com>
2026-09-17 05:55:42 -0300

    feat: export NEXUS orchestration contracts

---
+++ forage/orchestration/__init__.py
@@ -0,0 +1,4 @@
+from forage.orchestration.base import ExecutionContext, SpecializedAgent
+from forage.orchestration.registry import AgentRegistry
+
+__all__ = ["AgentRegistry", "ExecutionContext", "SpecializedAgent"]
