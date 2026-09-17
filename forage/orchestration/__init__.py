c4b4ff65b5e52d95bad42572cd3e8489fb2c6f84
Humberto Freitas <162592407+FreitasCyberSec@users.noreply.github.com>
2026-09-17 05:56:33 -0300

    feat: export NEXUS orchestrator

--- forage/orchestration/__init__.py
+++ forage/orchestration/__init__.py
@@ -1,4 +1,11 @@
 from forage.orchestration.base import ExecutionContext, SpecializedAgent
+from forage.orchestration.orchestrator import NexusOrchestrator, UnknownAgentError
 from forage.orchestration.registry import AgentRegistry
 
-__all__ = ["AgentRegistry", "ExecutionContext", "SpecializedAgent"]
+__all__ = [
+    "AgentRegistry",
+    "ExecutionContext",
+    "NexusOrchestrator",
+    "SpecializedAgent",
+    "UnknownAgentError",
+]
